38f4cd091f4529384d0693a5e1e56204a951a4ac
dmcgrath19 <s2558433@ed.ac.uk>
2025-05-13 11:50:03 -0700

    update calendar UI

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function getIconPath() {
 function createWindow () {
   const win = new BrowserWindow({
     width: 1000,
-    height: 700,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
